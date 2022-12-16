1ac1d5598a5a08de376542b8c5bd708d823c09dd
Крылов Алексей <krylov1709@yandex.ru>
2022-12-16 22:07:19 +0300

    api_with_restrictions доработка

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/urls.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from advertisements.views import AdvertisementViewSet
 
 router = DefaultRouter()
-router.register('advertisements/', AdvertisementViewSet)
+router.register('advertisements', AdvertisementViewSet)
 
 
 urlpatterns = [
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,33 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from rest_framework.test import APIClient
+
+from students.models import Student, Course
+from model_bakery import baker
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+    return factory
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+    return factory
+
+
+@pytest.mark.django_db
+def test_create_course(client, course_factory):
+    course = course_factory()
+    response = client.get('api/v1/courses')
+    data = response.json()
+    assert data == course
+
+
